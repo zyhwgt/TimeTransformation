9fc2254b5e840ff87c18c4e465404b7a0c6555cb
zyh <zyh@DESKTOP-LNOIMMD>
2021-04-30 22:28:51 +0800

    添加项目文件。

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Hou = New System.Windows.Forms.TextBox()
+        Me.Days = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Sec = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Min = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(260, 175)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "转换"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Hou
+        '
+        Me.Hou.BackColor = System.Drawing.Color.White
+        Me.Hou.Location = New System.Drawing.Point(318, 76)
+        Me.Hou.Name = "Hou"
+        Me.Hou.ReadOnly = True
+        Me.Hou.Size = New System.Drawing.Size(100, 25)
+        Me.Hou.TabIndex = 1
+        '
+        'Days
+        '
+        Me.Days.BackColor = System.Drawing.Color.White
+        Me.Days.Location = New System.Drawing.Point(318, 107)
+        Me.Days.Name = "Days"
+        Me.Days.ReadOnly = True
+        Me.Days.Size = New System.Drawing.Size(100, 25)
+        Me.Days.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(275, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "小时"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(290, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "天"
+        '
+        'Sec
+        '
+        Me.Sec.BackColor = System.Drawing.Color.White
+        Me.Sec.Location = New System.Drawing.Point(96, 71)
+        Me.Sec.Name = "Sec"
+        Me.Sec.ReadOnly = True
+        Me.Sec.Size = New System.Drawing.Size(100, 25)
+        Me.Sec.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(62, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(22, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "秒"
+        '
+        'Min
+        '
+        Me.Min.BackColor = System.Drawing.Color.White
+        Me.Min.Location = New System.Drawing.Point(96, 107)
+        Me.Min.Name = "Min"
+        Me.Min.ReadOnly = True
+        Me.Min.Size = New System.Drawing.Size(100, 25)
+        Me.Min.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 109)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "分钟"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"秒", "分", "时", "天"})
+        Me.ComboBox1.Location = New System.Drawing.Point(96, 30)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 23)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(318, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(230, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 15)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "要转换的值"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(17, 38)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(67, 15)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "转换类型"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(79, 173)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 32)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "清空"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 239)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Days)
+        Me.Controls.Add(Me.Sec)
+        Me.Controls.Add(Me.Min)
+        Me.Controls.Add(Me.Hou)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Hou As TextBox
+    Friend WithEvents Days As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Sec As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Min As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim min As String = Hou.Text.Trim
+        'Dim days As Double
+        'days = CDbl(min) / 24 / 60
+        'Me.Days.Text = Format(days, "0.00")
+        Dim Inval As String = TextBox1.Text
+        If Inval.Length = 0 Then
+            MsgBox("请输入要转换的值")
+            Exit Sub
+        End If
+        If ComboBox1.Text = "" Then
+            MsgBox("请选择要转换的内容")
+            Exit Sub
+        End If
+        Select Case ComboBox1.Text
+            Case "秒"
+                Sec.Text = Inval
+                Min.Text = Format(CDbl(Inval) / 60, "0.000")
+                Hou.Text = Format(CDbl(Inval) / 60 / 60, "0.000")
+                Days.Text = Format(CDbl(Inval) / 60 / 60 / 24, "0.000")
+
+            Case "分"
+                Sec.Text = Format(CDbl(Inval) * 60, "0.000")
+                Min.Text = Inval
+                Hou.Text = Format(CDbl(Inval) / 60, "0.000")
+                Days.Text = Format(CDbl(Inval) / 60 / 24, "0.000")
+
+            Case "时"
+                Sec.Text = Format(CDbl(Inval) * 60 * 60, "0.000")
+                Min.Text = Format(CDbl(Inval) * 60, "0.000")
+                Hou.Text = Inval
+                Days.Text = Format(CDbl(Inval) /  24, "0.000")
+            Case "天"
+                Hou.Text = Format(CDbl(Inval) * 24, "0.000")
+                Min.Text = Format(CDbl(Inval) * 60 * 60, "0.000")
+                Sec.Text = Format(CDbl(Inval) * 60 * 60 * 24, "0.000")
+                Days.Text = Inval
+        End Select
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ComboBox1.Text = ""
+        Days.Text = ""
+        Hou.Text = ""
+        Min.Text = ""
+        Sec.Text = ""
+        TextBox1.Text = ""
+    End Sub
+End Class
+
